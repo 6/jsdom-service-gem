4e7ac0af6b3092d2fc99b82a8d50452f16829cdf
Peter Graham <peter@1000memories.com>
2013-02-05 07:41:26 -0800

    Add some specs with pending error handling and implement a basic .run method

--- lib/jsdom_service.rb
+++ lib/jsdom_service.rb
@@ -1,2 +1,12 @@
+require "rest_client"
+require "json"
+
 module JSDOMService
+  module_function
+
+  def run(endpoint, html, code)
+    raw_json = RestClient.post(endpoint, {html: html, code: code})
+    json = JSON.parse(raw_json)
+    json['result']
+  end
 end
--- spec/jsdom_service_spec.rb
+++ spec/jsdom_service_spec.rb
@@ -1,4 +1,57 @@
 require "spec_helper"
 
 describe "JSDOMService" do
+  def stub!(attributes = {})
+    attributes[:endpoint] ||= "http://example.com"
+
+    response_json = {
+      error: attributes[:error] || nil,
+      result: attributes[:result] || {},
+    }.to_json
+
+    stub_request(:post, attributes[:endpoint]).to_return(status: attributes[:status] || 200, body: response_json)
+  end
+
+  def go!(options = {})
+    endpoint = options[:endpoint] || "http://example.com"
+    html = options[:html] || "<h1>lol</h1>"
+    code = options[:code] || ""
+    JSDOMService.run(endpoint, html, code)
+  end
+
+  describe ".run" do
+    it "makes a POST request to the JSDOM service endpoint" do
+      endpoint_stub = stub!(endpoint: "http://example.com/endpoint")
+
+      go!(endpoint: "http://example.com/endpoint")
+      endpoint_stub.should have_been_requested
+    end
+
+    context "if the service responds with an error" do
+      it "raises a JSDOMService::ResponseException" do
+        pending
+      end
+    end
+
+    context "if the returned JSON includes an error" do
+      it "raises a JSDOMService::ParseException" do
+        pending
+      end
+    end
+
+    context "if the service responds with 200 OK and no JSON error" do
+      it "does not raise an exception" do
+        stub!(status: 200, error: nil)
+
+        expect { go! }.not_to raise_error
+      end
+
+      it "returns parsed JSON response" do
+        stub!(:result => {'some' => 'json'})
+
+        result = go!
+        result.should == {'some' => 'json'}
+      end
+    end
+  end
 end
